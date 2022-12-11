d7105a6e8c5ec614bd529ec1d734082de193bbdb
Xavier Leftridge <111714709+xavL559@users.noreply.github.com>
2022-12-10 20:36:29 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+"what worked well for me during these nine weeks was defitenly the notes from the book and help from Mr. Cruz");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log(
+  "i dont know what exactly i would change about this course because everything was perfect, the instructor helped a lot and the notes were really good the only thing i would change is the class would be longer so i could learn more.");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log(
+  "Yes i did learn everything i expected to learn about javascript like how to loop and transfer ids from html to js and how to use math.");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "Everything in this class was new to me because i havent learned js yet or even looked at it until this class.");
